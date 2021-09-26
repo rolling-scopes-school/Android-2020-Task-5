62684d6d1d45c1510c6935c38fba686c2b34fc01
Rovenhook <stuchalka8@gmail.com>
2021-09-26 13:13:03 +0300

    init commit

---
+++ app/src/main/java/com/rovenhook/rsshool2021_android_task_catapi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rovenhook.rsshool2021_android_task_catapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
