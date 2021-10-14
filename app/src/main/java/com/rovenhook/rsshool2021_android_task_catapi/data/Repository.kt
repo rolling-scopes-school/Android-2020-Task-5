b6fcba5107fffc967f321b28833df9ff9968533e
Rovenhook <79100399+Rovenhook@users.noreply.github.com>
2021-10-14 16:33:38 +0300

    Clear master (#3)
    * delete
    
    * delete
    
    * delete
    
    * Delete path/to directory
    
    * Delete .gitignore
    
    * Delete build.gradle.kts
    
    * Delete settings.gradle.kts
    
    * Delete gradle.properties
    
    * Delete gradlew
    
    * Delete gradlew.bat

--- app/src/main/java/com/rovenhook/rsshool2021_android_task_catapi/data/Repository.kt
+++
@@ -1,5 +0,0 @@
-package com.rovenhook.rsshool2021_android_task_catapi.data
-
-class Repository {
-    suspend fun getMoreCats(page: Int) = CatApiImplementation.getAllCats(page)
-}
--- app/src/main/java/com/rovenhook/rsshool2021_android_task_catapi/listeners/OnSmallImageClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.rovenhook.rsshool2021_android_task_catapi.listeners
-
-import android.widget.ImageView
-
-interface OnSmallImageClickListener {
-    fun onSmallImageClick(imageView: ImageView)
-}
